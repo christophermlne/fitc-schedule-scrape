ef04f30fc3ac24ac9d8125b54d264c201633764d
Christopher Milne <christopher.milne@gmail.com>
2014-04-27 18:20:46 -0300

    content fed into items

--- scrape.rb
+++ scrape.rb
@@ -13,18 +13,30 @@ class SessionsScrape
   end
 
   def build_items
-    @rows.each do |row|
-      timebox = row.css('.timebox')
-      starttime = timebox.css('.starttime').text
-      endtime = timebox.css('.endtime').text
 
-      sessions = row.css('.sbox')
-      sessions.each do |session|
-        title    = session.css('.title').text.strip
-        speakers = session.css('.speakers').text.strip
-        puts "Title: #{title}"
-        puts "Speakers: #{speakers}"
+    items = Array.new
+    $id = 0
+    @rows.each do |row|
+      timebox    = row.css('.timebox')
+      starttime  = timebox.css('.starttime').text
+      endtime    = timebox.css('.endtime').text
+      sessions   = row.css('.sbox')
+
+      sessions.each do |s|
+        item = Hash.new
+        item[:id]        = $id
+        item[:title]     = s.css('.title').text.strip
+        item[:speakers]  = s.css('.speakers').text.strip
+        item[:starttime] = starttime
+        item[:endtime]   = endtime
+        # datadump[id][title] => title
+        items << item
+        $id += 1
       end
+    end
+    items.each do |item|
+      puts "#{item[:id]} #{item[:title]}"
+      puts "\tStarts: #{item[:starttime]} Ends: #{item[:endtime]}\n"
     end
   end
 
